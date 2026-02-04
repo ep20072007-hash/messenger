18f88661dd00f4db97b8f5726400634763eb5275
ep20072007 <ep20072007@gmail.com>
2026-02-04 22:09:02 +0300

    fix render port

--- server/index.js
+++ server/index.js
@@ -73,6 +73,9 @@ io.on("connection", socket => {
 
 const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, "0.0.0.0", () => {
   console.log("SERVER STARTED ON " + PORT);
 });
+
